bd569b9629844c305d15069613300c93c4a48444
PC-I7\b_zam <b_zamir@mail.ru>
2018-08-04 15:48:54 +0300

    wheel change value (float)

--- src/Urho3DMaterialEditor/Views/ScalarNodeView.xaml.cs
+++ src/Urho3DMaterialEditor/Views/ScalarNodeView.xaml.cs
@@ -11,5 +11,13 @@ namespace Urho3DMaterialEditor.Views
         {
             InitializeComponent();
         }
+
+        private void ScalarTextBox_MouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e) {
+            var txt = sender as TextBox; if (txt == null) return;
+            float fl1 = 0;
+            var st = float.TryParse(txt.Text.Replace('.',','),out fl1);
+            if (st) txt.Text = (fl1 + (e.Delta<0?-.1f:.1f)).ToString().Replace(',', '.');
+            
+        }
     }
 }
