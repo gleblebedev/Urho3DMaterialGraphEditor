036bd46958576b03184b18bb172f19d5a7ad5387
ZWEAR\MSasus <b_zamir@mail.ru>
2018-08-06 11:42:18 +0300

    Slider wheel control

---
+++ src/Urho3DMaterialEditor/Views/sliderControl.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Urho3DMaterialEditor.Views {
+    class SliderControl: Slider {
+        private string _path;
+
+
+        public SliderControl() {
+            Orientation = Orientation.Horizontal;
+            Minimum = 0;
+            Maximum = 1;
+        }
+
+        public string Path {
+            get => _path;
+            set {
+                if (_path != value) {
+                    _path = value;
+                    CreateBinding();
+                }
+            }
+        }
+
+        private void CreateBinding() {
+            BindingOperations.SetBinding(this, ValueProperty, new Binding {
+                Path = new PropertyPath(_path),
+                UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged,
+                ValidatesOnNotifyDataErrors = true,
+                ValidatesOnDataErrors = true,
+                NotifyOnValidationError = true,
+                ValidationRules =
+                {
+                    new NumericValidationRule {ValidatesOnTargetUpdated = true, ValidationType = typeof(float)}
+                }
+
+            });
+        }
+        protected override void OnMouseWheel(MouseWheelEventArgs e) {
+            if (this is Slider sl) {
+                sl.Value = Urho.MathHelper.Clamp((float)(sl.Value + (e.Delta > 0 ? .1f : -.1f)), 0, 1);
+                e.Handled = true;
+            }
+        }
+       
+    }
+}
