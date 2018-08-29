f50bfe0ba0e5bb5b202cb231aabb5c20c38acfdb
Gleb <gleb@gleblebedev.com>
2018-08-29 22:08:20 +0100

    function

---
+++ src/Urho3DMaterialEditor/Views/FunctionNodeView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Urho3DMaterialEditor.Views
+{
+    /// <summary>
+    ///     Interaction logic for FunctionNodeView.xaml
+    /// </summary>
+    public partial class FunctionNodeView : UserControl
+    {
+        public FunctionNodeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
