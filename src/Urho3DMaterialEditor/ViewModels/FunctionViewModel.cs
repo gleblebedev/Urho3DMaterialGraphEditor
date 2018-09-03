df2be0c98e16a655c38b099de4418ee9c077ce02
ZWEAR\MSasus <b_zamir@mail.ru>
2018-09-03 17:57:30 +0300

    generate text of function

--- src/Urho3DMaterialEditor/ViewModels/FunctionViewModel.cs
+++ src/Urho3DMaterialEditor/ViewModels/FunctionViewModel.cs
@@ -11,7 +11,12 @@ namespace Urho3DMaterialEditor.ViewModels
     {
         private readonly IDisposable _subscription;
         private string _value;
-        public string funcHeader;
+        public string funcHeader, funcFoot, functionTXT;
+
+        public string FuncHeader {
+            get => funcHeader;
+            set => RaiseAndSetIfChanged(ref funcHeader, value);
+        }
 
         public FunctionViewModel(ScriptViewModel script, ScriptNode node) : base(script, node)
         {
@@ -20,23 +25,28 @@ namespace Urho3DMaterialEditor.ViewModels
                 .ObserveOnDispatcher()
                 .Subscribe(_ => BuildNodeValue());
             CanRename = true;
+
+            CreateFuncTXT();
         }
 
         internal void AddPin(string val) {
             var np = new PinWithConnection(val,val);
             Node.InputPins.Add(np);
             InputPins.Add(new InputPinViewModel(this, np));
-            CreateFuncHead();
+            CreateFuncTXT();
         }
 
-        public void CreateFuncHead() {
-            funcHeader = "(";
+        public void CreateFuncTXT() {
+            var outP = Node.OutputPins[0]?.Type;
+            FuncHeader = outP + " " + Name + " (";
             int nn = 1;
             foreach (var item in Node.InputPins) {
-                funcHeader += item.Type + " var" + nn.ToString()+", ";
+                FuncHeader += item.Type + " var" + nn.ToString()+", ";
                 nn++;
             }
-            funcHeader = funcHeader.Substring(0, funcHeader.Length - 2)+")";
+            FuncHeader = FuncHeader.Substring(0, FuncHeader.Length - 2)+")";
+
+            functionTXT = FuncHeader + "\n{\n" + (Value??"") + "\n" + "return "+ funcFoot+ ";\n}";
         }
 
         internal void OutPin(string val) {
@@ -45,17 +55,22 @@ namespace Urho3DMaterialEditor.ViewModels
             Node.OutputPins.Add(np);
             OutputPins.Clear();
             OutputPins.Add(new OutputPinViewModel(this, np));
+
+            CreateFuncTXT();
         }
 
         public string EditableValue
         {
             get => _value;
-            set => RaiseAndSetIfChanged(ref _value, value);
+            set => 
+                RaiseAndSetIfChanged(ref _value, value);
         }
 
         internal void ClearInPins() {
             Node.InputPins.Clear();
             InputPins.Clear();
+
+            FuncHeader = "";
         }
 
         public void Dispose()
@@ -67,5 +82,6 @@ namespace Urho3DMaterialEditor.ViewModels
         {
             Value = _value;
         }
+
     }
 }
--- src/Urho3DMaterialEditor/Views/FunctionNodeView.xaml.cs
+++ src/Urho3DMaterialEditor/Views/FunctionNodeView.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
-using System.ComponentModel;
 using System.Windows.Controls;
 using Urho3DMaterialEditor.Model;
+
+using System.Windows;
+using System.Windows.Input;
+using Toe.Scripting.WPF.ViewModels;
+using Toe.Scripting.WPF.Views;
 
 namespace Urho3DMaterialEditor.Views {
     /// <summary>
@@ -15,6 +19,8 @@ namespace Urho3DMaterialEditor.Views {
         public FunctionNodeView()
         {
             InitializeComponent();
+            LayoutUpdated += UpdateSize;
+
             // FillMenuPins();
         }
 
@@ -43,5 +49,94 @@ namespace Urho3DMaterialEditor.Views {
             var dc = DataContext as ViewModels.FunctionViewModel;
             dc.ClearInPins();
         }
+
+        private void funcFoot_LostFocus(object sender, RoutedEventArgs e) {
+            var dc = DataContext as ViewModels.FunctionViewModel;
+            dc.funcFoot = funcFoot.Text;
+            dc.CreateFuncTXT();
+        }
+
+        //_______________________________________DRAg & DROP
+
+        public static readonly DependencyProperty AdditionalContentProperty =
+               DependencyProperty.Register("NodeContent", typeof(object), typeof(FunctionNodeView),
+                   new PropertyMetadata(null));
+
+        public static readonly DependencyProperty CanRenameProperty =
+              DependencyProperty.Register("CanRename", typeof(bool), typeof(FunctionNodeView),
+                   new PropertyMetadata(false));
+
+        private bool _dragged;
+        private Canvas _parent;
+        private Point _prevPos;
+        private bool _wasSelected;
+
+        public object NodeContent {
+            get => GetValue(AdditionalContentProperty);
+            set => SetValue(AdditionalContentProperty, value);
+        }
+
+        public bool CanRename {
+            get => (bool)GetValue(CanRenameProperty);
+            set => SetValue(CanRenameProperty, value);
+        }
+
+        protected NodeViewModel ViewModel => DataContext as NodeViewModel;
+
+        protected void StartDragging(object sender, MouseButtonEventArgs e) {
+            ViewUtils.FindFocusableParent(this)?.Focus();
+            if (ViewModel == null)
+                return;
+            _wasSelected = ViewModel.IsSelected;
+            if (!_wasSelected)
+                if ((Keyboard.Modifiers & ModifierKeys.Control) != 0)
+                    ViewModel.Script.AddToSelection(ViewModel);
+                else
+                    ViewModel.Script.Select(ViewModel);
+
+            _dragged = false;
+            _parent = _parent ?? ViewUtils.FindCanvasParent(this);
+            _prevPos = e.GetPosition(_parent);
+            ((UIElement)sender).CaptureMouse();
+            ((UIElement)sender).MouseMove += Drag;
+            ((UIElement)sender).MouseLeftButtonUp += StopDragging;
+            e.Handled = true;
+        }
+
+        protected void StopDragging(object sender, MouseButtonEventArgs e) {
+            if (_dragged) {
+                ViewModel.Script.StopDragging();
+                if (!_wasSelected)
+                    ViewModel.Script.RemoveSelection(ViewModel);
+            }
+
+            ((UIElement)sender).MouseMove -= Drag;
+            ((UIElement)sender).MouseLeftButtonUp -= StopDragging;
+            ((UIElement)sender).ReleaseMouseCapture();
+            e.Handled = true;
+        }
+
+        protected void Drag(object sender, MouseEventArgs e) {
+            if (!_dragged) {
+                _dragged = true;
+                ViewModel.Script.StartDragging();
+            }
+
+            var pos = e.GetPosition(_parent);
+            ViewModel.Script.MoveSelectedNodes(pos - _prevPos);
+            _prevPos = pos;
+        }
+
+        protected void UpdateSize(object sender, EventArgs e) {
+            var viewModel = ViewModel;
+            if (viewModel != null) viewModel.Size = new Size(ActualWidth, ActualHeight);
+        }
+
+        private void SelectIfNotSelected(object sender, MouseButtonEventArgs e) {
+            var viewModel = ViewModel;
+            if (viewModel != null && !viewModel.IsSelected) viewModel.Script.Select(viewModel);
+        }
+
+
     }
 }
