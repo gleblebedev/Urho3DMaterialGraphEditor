e09988437c19589230ed22ee767caed73d2067e3
ZWEAR\MSasus <b_zamir@mail.ru>
2018-09-07 18:02:22 +0300

    error

--- src/Urho3DMaterialEditor/Views/CodeEditor.xaml.cs
+++ src/Urho3DMaterialEditor/Views/CodeEditor.xaml.cs
@@ -19,71 +19,102 @@ namespace Urho3DMaterialEditor.Views {
     /// <summary>
     /// Логика взаимодействия для CodeEditor.xaml
     /// </summary>
-    public partial class CodeEditor : UserControl {
-        private string _path;
+    // public partial class CodeEditor : UserControl {
+    //private string _path;
 
-        public string Path {
-            get => _path;
-            set {
-                if (_path != value) {
-                    _path = value;
-                    CreateBinding();
+    //public string Path {
+    //    get => _path;
+    //    set {
+    //        if (_path != value) {
+    //            _path = value;
+    //            CreateBinding();
+    //        }
+    //    }
+    //}
+
+    //private void CreateBinding() {
+    //    BindingOperations.SetBinding(this, addprop, new Binding {
+    //        Path = new PropertyPath(_path),
+    //        UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged,
+    //       // ValidatesOnNotifyDataErrors = true,
+    //       // ValidatesOnDataErrors = true,
+    //       // NotifyOnValidationError = true,
+    //    });
+    //}
+
+    //public string Text {
+    //    get {
+    //        return textBox.Text;
+    //    }
+    //    set {
+    //        textBox.Text = value;
+    //    }
+    //}
+
+    //public DependencyProperty TextProperty { get; private set; }
+
+    //public static readonly DependencyProperty addprop =
+    // DependencyProperty.Register("TxT", typeof(string), typeof(CodeEditor),
+    //     new PropertyMetadata(null));
+
+    //public object TxT {
+    //    get =>(string) GetValue(addprop);
+    //    set => SetValue(addprop, value);
+    //}
+
+    //public FastColoredTextBox textBox;
+    //WindowsFormsHost host;
+    //public CodeEditor() {
+    //    InitializeComponent();
+
+    //    host = new WindowsFormsHost();
+    //    textBox = new FastColoredTextBox();
+    //    host.Child = textBox;
+
+    //    grid.Children.Add(host);
+    //    textBox.TextChanged += Ts_TextChanged;
+
+    //    //textBox.ForeColor =System.Drawing.Color.White;
+
+
+    //    // textBox.Text = "public class Hello {  }";
+
+    //  //  Text = "{Binding EditableValue, UpdateSourceTrigger=PropertyChanged}
+    //}
+
+    //private void Ts_TextChanged(object sender, FastColoredTextBoxNS.TextChangedEventArgs e) {
+
+    //}
+
+
+    // }
+
+    public class CodeTextboxHost : WindowsFormsHost {
+        private readonly FastColoredTextBox _innerTextbox = new FastColoredTextBox();
+
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(CodeTextboxHost), new PropertyMetadata("", new PropertyChangedCallback(
+            (d, e) => {
+                var textBoxHost = d as CodeTextboxHost;
+                if (textBoxHost != null && textBoxHost._innerTextbox != null) {
+                    textBoxHost._innerTextbox.Text = textBoxHost.GetValue(e.Property) as string;
                 }
-            }
+            }), null));
+
+        public CodeTextboxHost() {
+            Child = _innerTextbox;
+
+            _innerTextbox.TextChanged += _innerTextbox_TextChanged;
         }
 
-        private void CreateBinding() {
-            BindingOperations.SetBinding(this, addprop, new Binding {
-                Path = new PropertyPath(_path),
-                UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged,
-               // ValidatesOnNotifyDataErrors = true,
-               // ValidatesOnDataErrors = true,
-               // NotifyOnValidationError = true,
-            });
+        private void _innerTextbox_TextChanged(object sender, FastColoredTextBoxNS.TextChangedEventArgs e) {
+            SetValue(TextProperty, _innerTextbox.Text);
         }
 
         public string Text {
-            get {
-                return textBox.Text;
-            }
+            get { return (string)GetValue(TextProperty); }
             set {
-                textBox.Text = value;
+                SetValue(TextProperty, value);
             }
-        }
-
-        public DependencyProperty TextProperty { get; private set; }
-
-        public static readonly DependencyProperty addprop =
-         DependencyProperty.Register("TxT", typeof(string), typeof(CodeEditor),
-             new PropertyMetadata(null));
-
-        public object TxT {
-            get =>(string) GetValue(addprop);
-            set => SetValue(addprop, value);
-        }
-
-        public FastColoredTextBox textBox;
-        WindowsFormsHost host;
-        public CodeEditor() {
-            InitializeComponent();
-
-            host = new WindowsFormsHost();
-            textBox = new FastColoredTextBox();
-            host.Child = textBox;
-
-            grid.Children.Add(host);
-            textBox.TextChanged += Ts_TextChanged;
-
-            //textBox.ForeColor =System.Drawing.Color.White;
-            
-
-            // textBox.Text = "public class Hello {  }";
-            
-          //  Text = "{Binding EditableValue, UpdateSourceTrigger=PropertyChanged}
-        }
-
-        private void Ts_TextChanged(object sender, FastColoredTextBoxNS.TextChangedEventArgs e) {
-           
         }
     }
 }
